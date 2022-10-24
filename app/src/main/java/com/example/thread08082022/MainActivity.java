a75b00cb11196bd0413aac488e8ae865d15fcb14
Phat <phatdroid94@gmail.com>
2022-10-24 21:00:37 +0700

    da luong

--- app/src/main/java/com/example/thread08082022/MainActivity.java
+++ app/src/main/java/com/example/thread08082022/MainActivity.java
@@ -10,41 +10,45 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a, b, c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        a = b = c;
+
        // Tạo thread
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                printMessage("A ");
+                for (int i = 1; i <= 10; i++) {
+                    a = i;
+                }
             }
         });
 
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                printMessage("B ");
+                for (int i = 1; i <= 10; i++) {
+                    b = i;
+                }
+            }
+        });
+
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 10; i++) {
+                    c = a + b;
+                }
             }
         });
 
         threadB.start();
         threadA.start();
+        threadC.start();
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("BBB","State A " + threadA.getState().toString());
-                Log.d("BBB","State B " + threadB.getState().toString());
-            }
-        },1);
-    }
-
-    private synchronized void printMessage(String message) {
-        for (int i = 0; i < 1000; i++) {
-            Log.d("BBB", message + i);
-        }
     }
 }
---
+++ app/src/main/java/com/example/thread08082022/MyFlag.java
@@ -0,0 +1,12 @@
+package com.example.thread08082022;
+
+/**
+ * Created by pphat on 10/24/2022.
+ */
+public class MyFlag {
+    int count;
+
+    public MyFlag(int count) {
+        this.count = count;
+    }
+}
